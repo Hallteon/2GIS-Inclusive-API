9efea2371d28d55bc0f60db7dd811ac6d990354b
Ivan Belogurov <belogurov.ivan@list.ru>
2025-10-04 14:49:14 +0300

    fix: fixed warnings route

--- api/routers/all_disabled_router.py
+++ api/routers/all_disabled_router.py
@@ -1,14 +1,12 @@
 import logging
-
 from typing import List
 
 from http import HTTPStatus
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, HTTPException, Depends, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.database import get_async_session
 from api.services.external.warning_service import WarningService
-from api.schemas.all_disabled_schemes import WarningGetScheme
 
 
 router = APIRouter(prefix='/disabled',
@@ -18,11 +16,12 @@ logger = logging.getLogger(__name__)
 
 
 @router.get('/warnings/get', response_model=List[str])
-async def get_warnings(warning_point: WarningGetScheme,
+async def get_warnings(latitude: str = Query(description='Широта'),
+                       longitude: str = Query(description='Долгота'),
                        session: AsyncSession = Depends(get_async_session)):
     try:
-        weather_warnings = await WarningService().get_weather_warnings(lat=warning_point.latitude,
-                                                                       lon=warning_point.longitude)
+        weather_warnings = await WarningService().get_weather_warnings(lat=float(latitude),
+                                                                       lon=float(longitude))
 
         return weather_warnings
 
